e9d2381c5faf6ea49f47e67a9901d11f82fa0e92
Antony <43699659+anton-srgv@users.noreply.github.com>
2025-03-31 23:17:08 +0300

    Add human and computer inputs Create variables to store score Start round function

--- myScript.js
+++ myScript.js
@@ -1,1 +1,37 @@
-console.log("Hello World!");
+console.log("Rock-paper-scissors Battle Begin!");
+
+//Rock-paper-scissors game:
+//Three values: rock, paper, scissor
+//System randomly picks one of three values
+function getComputerChoice () {
+    const choice = ["rock", "paper", "scissors"];
+    return choice[Math.floor(Math.random()*choice.length)];
+}
+
+//User inputs one of three values
+function getHumanChoice () {
+    const choice = prompt("Enter your choice: rock, paper, scissors");
+    return choice;
+}
+
+//Track score
+const humanScore = 0;
+const computerScore = 0;
+
+//Play a single round
+function playRound () {
+    
+}
+
+console.log(getHumanChoice());
+
+
+
+//System compares user's value with it's own:
+// IF Rock > Scissor and Rock < Paper
+// IF Scissor > Paper and Scissor < Rock
+// IF Paper > Rock and Paper < Scissor
+//IF user wins – congrats
+//If system wins – sad
+//If tie – funny
+//After each game offer another one
